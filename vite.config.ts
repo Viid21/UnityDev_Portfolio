2dcbeac5436cc1bddc4314eae163aaa9e71f26f0
David Rey <164158488+Viid21@users.noreply.github.com>
2026-02-20 13:01:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "UnityDev_Portfolio",
+  base: "/UnityDev_Portfolio/",
   plugins: [
     react({
       babel: {
